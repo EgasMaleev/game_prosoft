b0b23748dfdbcb55a8aeb930d24fc51139e2629e
Egor Maleev <e9126515199@icloud.com>
2024-04-11 23:25:03 +0500

    new one

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -29,7 +29,7 @@ MainWindow::MainWindow(QWidget *parent)
             if (newBtn->geometry().x() > width())  //чтобы работало в полноэкранном режиме
                 newBtn->deleteLater();
 
-            if (newBtn->geometry().y() + SizeOfButton > height())  //проигрыш
+            if (newBtn->geometry().y() + SizeOfButton > height())  //Проигрыш
             {
                 setWindowTitle("YOU LOOSE");
                 setStyleSheet("background-color: red");
